a144f6b5d5967a16ea0b50468bb661590b449da9
Subramanian Murugan <sasubramanian_md@hotmail.com>
2024-02-20 10:38:50 +0530

    vue js compoents

---
+++ vue-legacy/src/components/RootComponent.js
@@ -0,0 +1,7 @@
+import WelcomeComponent from "./WelcomeComponent.js"
+
+export default {
+    name: 'Root',
+    components: { WelcomeComponent },
+    template: '<WelcomeComponent></WelcomeComponent>'
+}
